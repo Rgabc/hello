46b9f7305c3cd1f7cf16ba59526fd1ea9471defd
Rgabc <rohitgajbhiye4@gmail.com>
2024-05-22 14:34:56 +0530

    afshu

---
+++ script.js
@@ -0,0 +1,23 @@
+async function login() {
+    const email = document.getElementById('email').value;
+    
+     console.log(email)
+    // Check if the username and password match a stored user
+  
+    // const user = await storedUsers.find(
+        
+    //     (row) => {return row.email === email && row.password === password});
+    // console.log(user)
+   
+    if (email === "afsheen") {
+      alert(`welcome ${email} angel`);
+     
+      // You can redirect to another page or perform other actions after successful login
+     // window.location.href="./reservation/index.html"; // Replace with the actual path of your profile
+     const url="../log/index.html"
+     window.open(url, '_blank');
+    
+    } else { 
+      alert('unknown person and please write name in lower case');
+    }
+  }
